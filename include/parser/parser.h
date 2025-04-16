5766eb164bdd6fa788cbbfdcb7f921d97c626ad2
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-04-15 21:22:24 -0300

    Add initial boilerplate code for the 'parser.h' file.

---
+++ include/parser/parser.h
@@ -0,0 +1,14 @@
+// Copyright - Victor Miguel de Morais Costa
+#ifndef KUCHIKI_PARSER_PARSER_H
+#define KUCHIKI_PARSER_PARSER_H
+
+namespace kuchiki {
+namespace parser {
+
+class Parser {};
+
+} // namespace parser
+
+} // namespace kuchiki
+
+#endif
