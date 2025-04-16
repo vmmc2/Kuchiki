6251b084eb85b8f6dc18ca47e9e010ecaec6da09
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-04-15 21:22:01 -0300

    Add initial boilerplate code for the 'lexer.h' file.

---
+++ include/lexer/lexer.h
@@ -0,0 +1,14 @@
+// Copyright - Victor Miguel de Morais Costa
+#ifndef KUCHIKI_LEXER_LEXER_H
+#define KUCHIKI_LEXER_LEXER_H
+
+namespace kuchiki {
+namespace lexer {
+
+class Lexer {};
+
+} // namespace lexer
+
+} // namespace kuchiki
+
+#endif
