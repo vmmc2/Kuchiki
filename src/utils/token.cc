fcc56f5a12bc1e5a2572e6602ef67594b60e7b01
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-04-15 21:20:31 -0300

    Add namespace declarations, class constructors and some class methods inside the 'token.cc' file.

---
+++ src/utils/token.cc
@@ -0,0 +1,24 @@
+// Copyright - Victor Miguel de Morais Costa
+
+#include "../../include/utils/token.h"
+
+namespace kuchiki {
+namespace utils {
+
+Token::Token(int line, int column, TokenType type, std::string lexeme,
+             std::any value)
+    : line_{line}, column_{column}, type_{type}, lexeme_{lexeme},
+      value_{value} {}
+
+int Token::line() { return line_; }
+
+int Token::column() { return column_; }
+
+TokenType Token::type() { return type_; }
+
+const std::string &Token::lexeme() { return lexeme_; }
+
+const std::any &Token::value() { return value_; }
+
+} // namespace utils
+} // namespace kuchiki
