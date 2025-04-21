ba4c2b6a69c2d04bd2dc36332e95d123a6d7618a
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-04-21 19:54:02 -0300

    Add a few changes to the 'main.cc' file.

--- src/main.cc
+++ src/main.cc
@@ -6,16 +6,15 @@
 #include "../include/utils/token.h"
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello, World!" << std::endl;
-  std::cout << "The value of 'argc' is: " << argc << std::endl;
-  std::cout << "The value of argv[1] is: " << argv[1] << std::endl;
-
-  kuchiki::utils::FileScanner file_scanner{argv[1]};
+  kuchiki::utils::FileScanner file_scanner{argv[2]};
   file_scanner.Scan();
 
   kuchiki::lexer::Lexer lexer{file_scanner.source_file_content()};
 
   std::vector<kuchiki::utils::Token> tokens = lexer.LexTokens();
+  if(lexer.found_lexing_error()){
+    return 1;
+  }
 
   for (auto &token : tokens) {
     std::cout << token << std::endl;
