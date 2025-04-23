a8af977426736245b290fa23dd11d97a3d2a0ecc
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-04-22 22:13:11 -0300

    Add initial code related to the 'Parser' class inside the 'parser.h' and 'parser.cc' files.

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -2,10 +2,20 @@
 #ifndef KUCHIKI_PARSER_PARSER_H
 #define KUCHIKI_PARSER_PARSER_H
 
+#include <vector>
+
+#include "../utils/token.h"
+
 namespace kuchiki {
 namespace parser {
 
-class Parser {};
+class Parser {
+public:
+  Parser(const std::vector<kuchiki::utils::Token> &tokens);
+
+private:
+  const std::vector<kuchiki::utils::Token> tokens_;
+};
 
 } // namespace parser
 
--- src/parser/parser.cc
+++ src/parser/parser.cc
@@ -1,1 +1,11 @@
 // Copyright - Victor Miguel de Morais Costa
+
+#include "../../include/parser/parser.h"
+
+namespace kuchiki {
+namespace parser {
+
+Parser::Parser(const std::vector<kuchiki::utils::Token> &tokens)
+    : tokens_{tokens} {}
+} // namespace parser
+} // namespace kuchiki
