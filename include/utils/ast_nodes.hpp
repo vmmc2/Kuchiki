f04f242d8b746c6b462bf55255c6b9f07edb9b77
Victor Miguel de Morais Costa <vmmc5910@gmail.com>
2025-05-26 22:18:15 -0300

    Add an interface class called 'IAstVisitor' and a 'AstPrinter' class. Also add implementation details for the other classes present inside the 'ast_nodes.hpp' file.

--- include/utils/ast_nodes.hpp
+++ include/utils/ast_nodes.hpp
@@ -3,6 +3,7 @@
 
 #include <any>
 #include <memory>
+#include <string>
 
 #include "token.hpp"
 
@@ -10,7 +11,7 @@ namespace kuchiki {
 
 namespace utils {
 
-class ASTNode;
+class AstNode;
 class ConstantExpressionNode;
 class ExpressionNode;
 class FunctionNode;
@@ -18,57 +19,111 @@ class ProgramNode;
 class ReturnStatementNode;
 class StatementNode;
 
-class ASTNode {
+class IAstVisitor {
 public:
-  virtual ~ASTNode() = default;
-
-  virtual void Print(int identation = 0) const = 0;
+  virtual std::any
+  VisitConstantExpressionNode(std::shared_ptr<ConstantExpressionNode> node) = 0;
+  virtual std::any
+  VisitExpressionNode(std::shared_ptr<ExpressionNode> node) = 0;
+  virtual std::any VisitFunctionNode(std::shared_ptr<FunctionNode> node) = 0;
+  virtual std::any VisitProgramMnode(std::shared_ptr<ProgramNode> node) = 0;
+  virtual std::any
+  VisitReturnStatementNode(std::shared_ptr<ReturnStatementNode> node) = 0;
+  virtual std::any VisitStatementNode(std::shared_ptr<StatementNode> node) = 0;
 };
 
-class ExpressionNode : public ASTNode {
+class AstNode {
 public:
-  void Print(int indentation = 0) const override = 0;
+  virtual ~AstNode() = default;
+
+  virtual std::any Accept(IAstVisitor &visitor) = 0;
 };
 
-class FunctionNode : public ASTNode {
+class ExpressionNode : public AstNode {
+public:
+  std::any Accept(IAstVisitor &visitor) override = 0;
+};
+
+class FunctionNode : public AstNode,
+                     public std::enable_shared_from_this<FunctionNode> {
 public:
   FunctionNode(kuchiki::utils::Token function_name,
-               std::unique_ptr<StatementNode> body);
-  void Print(int indentation = 0) const override = 0;
+               std::shared_ptr<StatementNode> body)
+      : function_name_{function_name}, body_{body} {}
+
+  std::any Accept(IAstVisitor &visitor) override {
+    return visitor.VisitFunctionNode(shared_from_this());
+  }
 
   const kuchiki::utils::Token function_name_;
-  const std::unique_ptr<StatementNode> body_;
+  const std::shared_ptr<StatementNode> body_;
 };
 
-class ProgramNode : public ASTNode {
+class ProgramNode : public AstNode,
+                    public std::enable_shared_from_this<ProgramNode> {
 public:
-  ProgramNode(std::unique_ptr<FunctionNode> function);
-  void Print(int indentation = 0) const override = 0;
+  ProgramNode(std::shared_ptr<FunctionNode> function) : function_{function} {}
 
-  const std::unique_ptr<FunctionNode> function_;
+  std::any Accept(IAstVisitor &visitor) override {
+    return visitor.VisitProgramMnode(shared_from_this());
+  }
+
+  const std::shared_ptr<FunctionNode> function_;
 };
 
-class StatementNode : public ASTNode {
+class StatementNode : public AstNode {
 public:
-  void Print(int indentation = 0) const override = 0;
+  std::any Accept(IAstVisitor &visitor) override = 0;
 };
 
-class ReturnStatementNode : public StatementNode {
+class ReturnStatementNode
+    : public StatementNode,
+      public std::enable_shared_from_this<ReturnStatementNode> {
 public:
   ReturnStatementNode(kuchiki::utils::Token keyword,
-                      std::unique_ptr<ConstantExpressionNode> expr);
-  void Print(int indentation = 0) const override {}
+                      std::shared_ptr<ConstantExpressionNode> expr)
+      : keyword_{keyword}, expr_{expr} {}
+
+  std::any Accept(IAstVisitor &visitor) override {
+    return visitor.VisitReturnStatementNode(shared_from_this());
+  }
 
   kuchiki::utils::Token keyword_;
   std::shared_ptr<ConstantExpressionNode> expr_;
 };
 
-class ConstantExpressionNode : public ExpressionNode {
+class ConstantExpressionNode
+    : public ExpressionNode,
+      public std::enable_shared_from_this<ConstantExpressionNode> {
 public:
-  ConstantExpressionNode(std::any value);
-  void Print(int indentation = 0) const override {}
+  ConstantExpressionNode(std::any value) : value_{value} {}
+
+  std::any Accept(IAstVisitor &visitor) {
+    return visitor.VisitConstantExpressionNode(shared_from_this());
+  }
 
   const std::any value_;
+};
+
+class AstPrinter : public IAstVisitor {
+  std::string Print(std::shared_ptr<AstNode> node) {
+    return std::any_cast<std::string>(node->Accept(*this));
+  }
+
+  std::any VisitConstantExpressionNode(
+      std::shared_ptr<ConstantExpressionNode> node) override {}
+
+  std::any VisitExpressionNode(std::shared_ptr<ExpressionNode> node) override {}
+
+  std::any VisitFunctionNode(std::shared_ptr<FunctionNode> node) override {}
+
+  std::any VisitProgramMnode(std::shared_ptr<ProgramNode> node) override {}
+
+  std::any
+  VisitReturnStatementNode(std::shared_ptr<ReturnStatementNode> node) override {
+  }
+
+  std::any VisitStatementNode(std::shared_ptr<StatementNode> node) override {}
 };
 
 } // namespace utils
